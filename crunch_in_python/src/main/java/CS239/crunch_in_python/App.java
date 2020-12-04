c7918f6cac3a3938a796cb543329fee73b87e5cb
Hao Zhuang <568736764zh@gmail.com>
2020-12-03 22:47:21 -0800

    Signed-off-by: Hao Zhuang <568736764zh@gmail.com>

---
+++ crunch_in_python/src/main/java/CS239/crunch_in_python/App.java
@@ -0,0 +1,13 @@
+package CS239.crunch_in_python;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
