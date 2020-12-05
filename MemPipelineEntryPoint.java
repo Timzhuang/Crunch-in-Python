af0dce98693147dba579e546dfb16afb1c4ea35c
Ishaan Pota <ishaan.pota@gmail.com>
2020-12-05 16:02:34 +0530

    my charger isnt workimhg rip

---
+++ MemPipelineEntryPoint.java
@@ -0,0 +1,29 @@
+import org.apache.crunch.Pipeline;
+import org.apache.crunch.impl.mem.MemPipeline;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import py4j.GatewayServer;
+
+public class MemPipelineEntryPoint {
+
+    private Pipeline p;
+
+    public MemPipelineEntryPoint() {
+
+        p = MemPipeline.getInstance();
+
+    }
+
+    public Pipeline getInstance() {
+
+        return p;
+        // Not sure if I should initialize in constructor or??
+    }
+
+    // Do i have to return all the static methods, not just getInstance???
+    public static void main(String[] args) {
+        GatewayServer gatewayServer = new GatewayServer(new MemPipelineEntryPoint());
+        gatewayServer.start();
+        System.out.println("Gateway Server Started");
+    }
+
+}
